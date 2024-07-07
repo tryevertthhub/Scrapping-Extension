2d9f8fb4425d752676cdcd3536bfeea412678e3c
tryevertthhub <tryevertth@gmail.com>
2024-07-07 10:36:26 +0900

    feat: add main function

--- background.js
+++ background.js
@@ -269,5 +269,270 @@ const URLS = [
     chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
       const tab_id = tabs[0].id;
       console.log("start");
+      const runScrap = async () => {
+        for (let i = 0; i < 100000000; i++) {
+          await gotoPage(googleUrl, "", tab_id);
+          let RATE_ = await getRate(tab_id);
+          await delay(1200);
+          const RATE = Number(RATE_);
+          console.log(RATE);
+    
+          const RATE1 = 192.83;
+          console.log(RATE1);
+    
+          for (const url of URLS) {
+            await gotoPage(PREFIX, url, tab_id);
+            await delay(3200 + 200 * Math.random());
+            await gotoPage(PREFIX, url, tab_id);
+            await delay(1300 + 200 * Math.random());
+    
+            let pageSize = await getPageSize(tab_id);
+            if (pageSize == undefined || pageSize == "undefined") pageSize = 1;
+            console.log(pageSize);
+            for (let pageIndex = 0; pageIndex < pageSize; pageIndex++) {
+              const pageUrl =
+                pageIndex == 0 ? `${url}` : `${url}?page=${pageIndex + 1}`;
+              await gotoPage(PREFIX, pageUrl, tab_id);
+              await delay(200 + 200 * Math.random());
+    
+              const productUrls = await getPageProducts(tab_id);
+              // console.log(productUrls);
+    
+              for (const product of productUrls) {
+                console.log("product page");
+                await gotoPage(PRODUCT_PREFIX, product, tab_id);
+    
+                await delay(1000 + 200 * Math.random());
+    
+                await clickButton(tab_id, "#menu-button-pdp-size-selector"); // all button
+                // await delay(200);
+    
+                await pressCMButton(tab_id);
+                // await delay(100);
+                await clickButton(tab_id, ".chakra-button.css-mh3eif");
+                // await delay(100);
+                //
+                const detailData1 = await getDetail1(tab_id);
+                const detailData2 = await getDetail2(tab_id);
+                let detailData;
+                if (detailData1 == 0) {
+                  detailData = detailData2;
+                }
+                if (detailData2 == 0) {
+                  detailData = detailData1;
+                }
+    
+                // console.log(detailData1, detailData2);
+                const PK = detailData[0][1];
+    
+                const size1 = await getSize(tab_id, "dt");
+                const size2 = await getSize(tab_id, "dd"); //chakra-stat__number css-1yv08e4 chakra-stat__number css-kezhfx
+    
+                const price2 = await getPrice(tab_id);
+                const imageUrl = await getImageUrl(tab_id, "#three-sixty-image");
+    
+                console.log(imageUrl);
+    
+                console.log("first start");
+    
+                await gotoPage(FIRSTSITE_PREFIX, PK, tab_id);
+                await delay(1000);
+                const imageUrl1 = await getImageUrl(tab_id, ".product-img");
+                // console.log(imageUrl1[0]);
+    
+                if (
+                  imageUrl1[0] == "undefined" ||
+                  imageUrl1[0] == undefined ||
+                  imageUrl1[0] == null ||
+                  imageUrl1[0] == ""
+                ) {
+                  console.log(
+                    "----------------------empty first page ------------------------------"
+                  );
+                  continue;
+                }
+    
+                const testdata = await test(tab_id, ".product-detail-info-table");
+                // console.log(testdata);
+    
+                const lowestprice = await getLowestPrice(tab_id);
+    
+                const productDescription = await getProductDescription(tab_id);
+    
+                await clickButton(tab_id, ".product-stock-label");
+                await delay(100);
+    
+                let FirstSize = await getsizeDetailFromTable(tab_id);
+                console.log(FirstSize);
+                if (FirstSize == undefined || FirstSize == null) FirstSize = [];
+    
+                //
+                if (Array.from(FirstSize).length > 0) {
+    
+                  await gotoPage(PRODUCT_PREFIX, product, tab_id);
+    
+                  await delay(500 + 200 * Math.random());
+    
+                  await clickButton(tab_id, "#menu-button-pdp-size-selector"); // all button
+                  await delay(100);
+    
+                  await pressCMButton(tab_id);
+                  await delay(100);
+                  const detailsizedata = await getSizeDetailData(
+                    tab_id,
+                    ".chakra-menu__menuitem-option.css-1uqbhsq"
+                  );
+    
+                  console.log("----@@@@----");
+                  console.log(detailsizedata);
+                  console.log(FirstSize);
+                  console.log("----@@@@----");
+    
+                  const newArrayOfConsistentItems = detailsizedata.reduce(
+                    (result, item) => {
+                      const numberFromFirstArray = parseFloat(
+                        item[2].split(" ")[1]
+                      );
+                      const matchingItemFromSecondArray = FirstSize.find(
+                        (secondItem) =>
+                          parseFloat(secondItem[0]) === numberFromFirstArray
+                      );
+    
+                      if (
+                        matchingItemFromSecondArray &&
+                        matchingItemFromSecondArray[1].trim() !== "出品待ち" &&
+                        !item[1].includes("--")
+                      ) {
+                        if (item[0].includes("$")) {
+                          result.push([
+                            Math.round(ChooseNumber(item[0]) * RATE),
+                            Math.round(ChooseNumber(item[1]) * RATE),
+                            item[2],
+                            ChooseNumber(matchingItemFromSecondArray[1]),
+                          ]);
+                        }
+                        else if (item[0].includes("£")) {
+                          result.push([
+                            Math.round(ChooseNumber(item[0]) * RATE1),
+                            Math.round(ChooseNumber(item[1]) * RATE1),
+                            item[2],
+                            ChooseNumber(matchingItemFromSecondArray[1]),
+                          ]);
+                        }
+                        else {
+                          result.push([
+                            ChooseNumber(item[0]),
+                            ChooseNumber(item[1]),
+                            item[2],
+                            ChooseNumber(matchingItemFromSecondArray[1]),
+                          ]);
+                        }
+                      }
+                      return result;
+                    },
+                    []
+                  );
+                  for (let i = 0; i < newArrayOfConsistentItems.length; i++) {
+                    console.log(
+                      newArrayOfConsistentItems[i][0],
+                      newArrayOfConsistentItems[i][1],
+                      newArrayOfConsistentItems[i][2],
+                      newArrayOfConsistentItems[i][3]
+                    );
+                  }
+    
+                  //-------- data processing   start   --------
+                  let finalResult = [];
+                  for (let i = 0; i < newArrayOfConsistentItems.length; i++) {
+                    let temp = newArrayOfConsistentItems[i];
+                    let saleMoney =
+                      Number(temp[0]) -
+                      Number(temp[0]) * 0.09 -
+                      Number(temp[0]) * 0.03 -
+                      1200;
+                    let hopeMoney =
+                      Number(temp[1]) -
+                      Number(temp[1]) * 0.09 -
+                      Number(temp[1]) * 0.03 -
+                      1200;
+    
+                    let minSaleMoney = saleMoney - temp[3]; //10
+                    let minSalePercent = (minSaleMoney / temp[3]) * 100; //11
+    
+                    let minHopeMoney = hopeMoney - temp[3]; //12
+                    let minHopePercent = (minHopeMoney / temp[3]) * 100; //13
+                    finalResult.push([
+                      minSaleMoney,
+                      minSalePercent,
+                      minHopeMoney,
+                      minHopePercent,
+                      temp[0],
+                      temp[3],
+                      temp[2]
+                      //
+    
+                    ]);
+                  }
+                  console.log(finalResult);
+                  //-------- data processing    end ------------
+    
+                  console.log("first end");
+                  //send data
+                  if (imageUrl1[0] && imageUrl[0] && finalResult.length > 0) {
+                    let maxSaleMoney = getMaximum(finalResult, 0);
+                    let maxSalePercent = getMaximum(finalResult, 1);
+                    let maxHopeMoney = getMaximum(finalResult, 2);
+                    let maxHopePercent = getMaximum(finalResult, 3);
+                    let _size1 = size1
+                      .slice(1)
+                      .filter((element) => element !== " ");
+                    let _size2 = size2
+                      .slice(1)
+                      .filter((element) => element !== " ");
+                    let _size = [];
+    
+                    for (let i = 0; i < _size1.length; i++) {
+                      const element = [_size1[i], _size2[i]];
+                      _size.push(element);
+                    }
+                    const shoesData = {
+                      ProductNumber: PK,
+                      ProductDescription: productDescription,
+                      date: testdata[2],
+                      image: imageUrl1[0][0] || imageUrl1[0][0],
+                      PriceData: finalResult,
+                      Lowest: lowestprice,
+                      MsaleMoney: maxSaleMoney,
+                      MSalePercent: maxSalePercent,
+                      MHopeMoney: maxHopeMoney,
+                      MHopePercent: maxHopePercent,
+                    };
+                    await sendData(shoesData);
+                  
+                  }
+                }
+              }
+            }
+          }
+        }
+      };
+      runScrap();
     });
   })
+
+  const sendData = async (shoesData) => {
+    await fetch("http://localhost:3003/api/v1/data/push", {
+      method: "POST",
+      body: JSON.stringify(shoesData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log("カートに追加されました:", response);
+      })
+      .catch((error) => {
+        console.error("カート追加エラー:", error);
+      });
+  };
+  
